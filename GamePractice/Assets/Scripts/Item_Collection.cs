2a5c60cbc52b4b112417faa5397aba09b1ae1d78
wql4612 <16604735985@163.com>
2025-05-08 15:47:52 +0800

    2d unity game practice

---
+++ GamePractice/Assets/Scripts/Item_Collection.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item_Collection : MonoBehaviour
+{
+    private int cherries = 0;
+    [SerializeField] private Text cherriesText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Cherry"))
+        {
+            Destroy(collision.gameObject);
+            cherries++;
+            cherriesText.text = "Cherries:" + cherries;
+        }
+    }
+}
---
+++ GamePractice/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //´¥Åöµ½ÏÝÚåËÀÍö
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
