053104d677c7ec9683caa238d28ab13d640644c7
starlightThD <148640677+starlightThD@users.noreply.github.com>
2025-05-11 23:47:53 +0800

    now DetectWall can move

--- GamePractice/Assets/Scripts/DetectWall.cs
+++ GamePractice/Assets/Scripts/DetectWall.cs
@@ -1,57 +1,90 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class DetectWall : MonoBehaviour
 {
-    [SerializeField]public GameObject square;
-    [SerializeField] private KeyCode key;
+    [SerializeField] public GameObject square;
+    [SerializeField] private KeyCode key = KeyCode.N;
     [SerializeField] public TextMeshPro textMesh;
+    [SerializeField] private Transform beginPos;
+    [SerializeField] private Transform endPos;
+    [SerializeField] private BoxCollider2D moveTrigger;
+    [SerializeField] private float speed = 3f;
 
-    // Start is called before the first frame update
+    private Vector3 targetPosition;
+    private bool isMoving;
+
     void Start()
     {
-        if (square == null)
+        InitializeComponents();
+        square.transform.position = beginPos.position;
+        targetPosition = beginPos.position;
+        isMoving = false;
+        if (textMesh != null) textMesh.text = key.ToString().ToUpper();
+    }
+
+    void Update()
+    {  
+        if (Input.GetKeyDown(key)) ToggleWallCollider(false);
+        if (Input.GetKeyUp(key)) ToggleWallCollider(true);
+
+        if (isMoving)
         {
-            square = transform.Find("Square").gameObject;
-        }
-        if(key == KeyCode.None)
-        {
-            Debug.Log("No key assigned! Set to KeyCode.N");
-            key = KeyCode.N;
-        }
-        else if(key < KeyCode.A || key > KeyCode.Z)
-        {
-            Debug.Log("Invalid key! Set to KeyCode.N");
-            key = KeyCode.N;
-        }
-        if (textMesh != null)
-        {
-            textMesh.text = key.ToString();
+            square.transform.position = Vector3.MoveTowards(
+                square.transform.position,
+                targetPosition,
+                speed * Time.deltaTime
+            );
+
+            if (Vector3.Distance(square.transform.position, targetPosition) < 0.01f)
+            {
+                isMoving = false;
+                moveTrigger.enabled = true; 
+                Debug.Log("Wall reached end position!");
+            }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (Input.GetKeyDown(key))
+        if (other.CompareTag("Player") && !isMoving)
         {
-            BoxCollider2D squareCollider = square.GetComponent<BoxCollider2D>();
-            if (squareCollider != null)
-            {
-                Debug.Log("Wall disabled!");
-                squareCollider.enabled = false;
-            }
-        }else if (Input.GetKeyUp(key))
-        {
-            BoxCollider2D squareCollider = square.GetComponent<BoxCollider2D>();
-            if (squareCollider != null)
-            {
-                Debug.Log("Wall enabled!");
-                squareCollider.enabled = true;
-            }
+            StartMovement();
         }
+    }
+
+    private void InitializeComponents()
+    {
+        // 自动获取未手动赋值的组件
+        if (!beginPos) beginPos = transform.Find("BeginPos");
+        if (!endPos) endPos = transform.Find("EndPos");
+        if (!moveTrigger) moveTrigger = GetComponent<BoxCollider2D>();
+        if (!square) square = transform.Find("Square").gameObject;
+
+        // 验证按键有效性
+        if (key < KeyCode.A || key > KeyCode.Z)
+        {
+            Debug.LogWarning("Invalid key! Defaulting to KeyCode.N");
+            key = KeyCode.N;
+        }
+    }
+
+    private void ToggleWallCollider(bool state)
+    {
+        var collider = square.GetComponent<BoxCollider2D>();
+        if (collider)
+        {
+            collider.enabled = state;
+            Debug.Log($"Wall {(state ? "enabled" : "disabled")}!");
+        }
+    }
+
+    private void StartMovement()
+    {
+        // 切换目标位置
+        targetPosition =endPos.position;
+        isMoving = true;
+        moveTrigger.enabled = false; 
+        Debug.Log("Wall moving!");
     }
 }
